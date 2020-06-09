d1d25bed5030a47a4dadb5b6b6a46f488079a1cf
nrcfrld <nrcfrld@gmail.com>
2020-06-09 21:41:36 +0700

    change baseurl

--- src/services/EventServices.js
+++ src/services/EventServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/nrcfrld/my-json-server',
+  baseURL: 'http://localhost:3000/events',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
